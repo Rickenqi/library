a6b47abe8d43193a014f934053288345b70f2d37
Rickenqi <rickenqi@outlook.com>
2020-06-14 00:28:12 +0800

    initialization

---
+++ src/main/java/com/ricky/library/demo/utils/type/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.ricky.library.demo.utils.type;
+
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/test/java/com/ricky/library/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricky.library.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
